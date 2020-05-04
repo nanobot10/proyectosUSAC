9f65cc05fd64738aab84ad9bcb0b1f12728c831c
dennis.castro <manuel.castro.guzman@ericsson.com>
2020-05-04 00:38:19 -0600

    navbar, login, logout

--- practica3-front-end/src/app/app-routing.module.ts
+++ practica3-front-end/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthGuardService } from './services/auth-guard.service';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { TransactionComponent } from './pages/transaction/transaction.component';
+import { CreditComponent } from './pages/credit/credit.component';
+import { DebitComponent } from './pages/debit/debit.component';
+import { BalanceComponent } from './pages/balance/balance.component';
 
 
 const routes: Routes = [
@@ -17,7 +21,22 @@ const routes: Routes = [
     path: 'home', component: HomeComponent,
     canActivate: [AuthGuardService]
   },
-  { path: '**', redirectTo: 'login' }
+  {
+    path: 'transaction', component: TransactionComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'credit', component: CreditComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'debit', component: DebitComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'balance', component: BalanceComponent,
+    canActivate: [AuthGuardService]
+  }
 ]
 
 
